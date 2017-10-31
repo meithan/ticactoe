555c61964a418cf0b43affa95e70a0a0dd95112a
Meithan <meithan@gmail.com>
2017-10-31 08:41:11 -0600

    Stub for NN trainer (genetic algorithm)

---
+++ NNTrainer.py
@@ -0,0 +1,8 @@
+# Trains a neural network to play the given game through a
+# genetic algorithm.
+
+# Receives a NNagent and uses a genetic algorithm to train it
+class NNTrainer:
+
+  def __init__(self, game, agent):
+    sell
